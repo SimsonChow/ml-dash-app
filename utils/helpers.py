8858df07c04a833f8a120a92d7204599567961d3
Wai Hang Chow Lin <wchowlin@contractor.usgs.gov>
2020-12-15 09:35:24 -0600

    test

--- utils/helpers.py
+++ utils/helpers.py
@@ -2,6 +2,21 @@ import pandas as pd
 import numpy as np
 import pickle
 
+def train_model(train_model, X_train, y_train):
+    """
+    Trains a classification model
+    Args:
+        model: sklearn estimator for classification
+        X_train: Model features training data values
+        y_train: Model target variable training data values
+    Returns:
+        model: trained classification model
+    """
+    model = train_model
+    model.fit(X_train, y_train)
+
+    return model
+
 def load_model(model, filepath):
     """
     Load model from output folder 
@@ -20,18 +35,4 @@ def load_model(model, filepath):
         trained_model = pickle.load(file)
     return trained_model
 
-def train_model(train_model, X_train, y_train):
-    """
-    Trains a classification model
-    Args:
-        model: sklearn estimator for classification
-        X_train: Model features training data values
-        y_train: Model target variable training data values
-    Returns:
-        model: trained classification model
-    """
-    model = train_model
-    model.fit(X_train, y_train)
-
-    return model
 
